48a9a3658b3762fb78ce576369a240a6f5dac7a5
Yamilet Martinez Delgado <st4546@utr.edu.mx>
2026-07-20 18:33:58 -0600

    feat: implement Helmet security middleware

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const helmet = require("helmet");
 const conectarDB = require("./src/config/database");
 const authRoutes = require("./src/routes/authRoutes");
 
 conectarDB();
 
 const app = express();
+app.use(helmet());
 
 // Puerto desde .env
 const port = process.env.PORT || 5100;
